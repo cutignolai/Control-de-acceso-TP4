c5fcebe987fee394fa937633077d3be9ed62ee75
cutignolai <58371858+cutignolai@users.noreply.github.com>
2022-11-09 23:13:49 -0300

    Se agregan driver tp1

---
+++ source/drivers/encoder.h
@@ -0,0 +1,57 @@
+/***************************************************************************//**
+  @file     encoder.h
+  @brief    Encoder Driver
+  @author   Micho
+ ******************************************************************************/
+#ifndef _ENCODER_H_
+#define _ENCODER_H_
+
+/*******************************************************************************
+ *                      INCLUDE HEADER FILES                                    *
+ ******************************************************************************/
+
+
+
+
+/*******************************************************************************
+ *          CONSTANT AND MACRO DEFINITIONS USING #DEFINE                        *
+ ******************************************************************************/
+
+#define ENCODER_DEV_MODE
+
+
+/*******************************************************************************
+ *               ENUMERATIONS AND STRUCTURES AND TYPEDEFS                       *
+ ******************************************************************************/
+typedef enum{
+  NONE_ENCODER = 0,
+  LEFT,
+  RIGHT
+}encoderEvent_t;
+
+
+
+/*******************************************************************************
+ *                  VARIABLE PROTOTYPES WITH GLOBAL SCOPE                       *
+ ******************************************************************************/
+
+/*******************************************************************************
+ *                   FUNCTION PROTOTYPES WITH GLOBAL SCOPE                      *
+ ******************************************************************************/
+
+
+void initEncoder();                           //Inicialización del encoder
+
+bool encoderGetStatus();                      //Me devuelve si hubo un cambio en la perilla
+
+bool encoderSetStatus(bool change_state);     //Me setea el status para que no quede en evento
+
+encoderEvent_t encoderGetEvent();             //Me devuelve si fue un giro hacia la derecha, izquierda, click o nada aun
+
+
+
+
+/*******************************************************************************
+ ******************************************************************************/
+
+#endif // _ENCODER_H_
